2d6229d7e8d63cf3dd4d8d491479d57487f6e570
Christoph Gruber <c.gruber@tum.de>
2023-08-15 19:44:56 +0200

    Added vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,8 @@
 import type { LoadEvent } from "@sveltejs/kit";
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
 
 export function load({ url }: LoadEvent) {
     return {
