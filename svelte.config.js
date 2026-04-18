62f02d43bd9450efd8e7495a2bf0ad7ea87d7b26
Christoph Gruber <Christoph.gruber2000@gmail.com>
2026-04-18 16:28:22 +0200

    add sveltekit instrumentation

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,14 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess({ script: true }),
   kit: {
+    experimental: {
+      tracing: {
+        server: true,
+      },
+      instrumentation: {
+        server: true,
+      },
+    },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
