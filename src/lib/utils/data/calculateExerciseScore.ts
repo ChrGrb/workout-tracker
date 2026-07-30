80cebdcee25647e23e8379958c874da98004617e
Christoph Gruber <Christoph.gruber2000@gmail.com>
2026-07-30 21:32:18 +0200

    fix: use all sets for exercise score

--- src/lib/utils/data/calculateExerciseScore.ts
+++ src/lib/utils/data/calculateExerciseScore.ts
@@ -2,9 +2,10 @@ import type { ExerciseFull } from "../prismaTypes";
 import { getExerciseSetScore } from "./getExerciseSetWeight";
 
 const calculateExerciseScore = (exercise: ExerciseFull) => {
-  return (exercise.sets ?? [])
-    .filter((set) => set.exerciseSetType === "WORKOUT")
-    .reduce((acc, set) => getExerciseSetScore(set) * set.reps + acc, 0);
+  return (exercise.sets ?? []).reduce(
+    (acc, set) => getExerciseSetScore(set) * set.reps + acc,
+    0,
+  );
 };
 
 export default calculateExerciseScore;
